3b8a07c16d15c331bb4c3233b225992b806cf3b4
Alexander <git@alexander-reiter.at>
2025-11-15 22:36:14 +0100

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
-import { Routes } from '@angular/router';
-import {AppComponent} from './app.component';
-import { HomeComponent } from './views/home/home.component';
-import { ProjectsComponent } from './views/projects/projects.component';
-import { BlogPostComponent } from './views/blog-post/blog-post.component';
-import { BlogComponent } from './views/blog/blog.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {ProjectsComponent} from './views/projects/projects.component';
+import {BlogComponent} from './views/blog/blog.component';
 
-export const routes: Routes = [
-  {path: '', component: HomeComponent},
+export const routes: Routes = [{path: '', component: HomeComponent},
 
-  {path: 'project', component: ProjectsComponent},
-  {path: 'project/:project', component: ProjectsComponent},
+	{path: 'project', component: ProjectsComponent},
 
-  {path: 'blog', component: BlogComponent},
-  {path: 'blog/:blog', component: BlogPostComponent},
+	{path: 'blog', component: BlogComponent}
 
 ];
