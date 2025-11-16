fe6eab810a0249359360575ee8970b4241275215
Alexander <git@alexander-reiter.at>
2025-11-16 15:08:28 +0100

    updated page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,6 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
-import {ProjectsComponent} from './views/projects/projects.component';
-import {BlogComponent} from './views/blog/blog.component';
 
-export const routes: Routes = [{path: '', component: HomeComponent},
-
-	{path: 'project', component: ProjectsComponent},
-
-	{path: 'blog', component: BlogComponent}
-
+export const routes: Routes = [
+	{path: '', component: HomeComponent}
 ];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,60 +1,35 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {HugeiconsIconComponent} from '@hugeicons/angular';
-import {
-	ArrowDown01Icon,
-	CrabIcon,
-	Database02Icon,
-	GithubIcon,
-	JavaIcon,
-	JavaScriptIcon,
-	Mail01Icon,
-	PythonIcon,
-	TwitchIcon,
-	VisualStudioCodeIcon
-} from '@hugeicons/core-free-icons';
 import {SnapScrollDirective} from '../../directives/snap-scroll.directive';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {
+	phosphorDatabase,
+	phosphorFileCSharp,
+	phosphorGithubLogo,
+	phosphorMailbox,
+	phosphorTwitchLogo
+} from '@ng-icons/phosphor-icons/regular';
+import {hugeJava, hugeJavaScript} from '@ng-icons/huge-icons';
+import {simplePython, simpleRust} from '@ng-icons/simple-icons';
 
 @Component({
 	selector: 'app-home',
-	imports: [RouterLink, HugeiconsIconComponent, SnapScrollDirective],
+	imports: [RouterLink, SnapScrollDirective, NgIcon],
 	templateUrl: './home.component.html',
-	styleUrl: './home.component.css'
+	styleUrls: ['./home.component.css'],
+	viewProviders: [
+		provideIcons({
+			phosphorGithubLogo,
+			phosphorTwitchLogo,
+			phosphorMailbox,
+			hugeJavaScript,
+			hugeJava,
+			simplePython,
+			simpleRust,
+			phosphorFileCSharp,
+			phosphorDatabase
+		})
+	]
 })
 export class HomeComponent {
-	protected readonly ArrowDown01Icon = ArrowDown01Icon;
-	protected readonly GithubIcon = GithubIcon;
-	protected readonly TwitchIcon = TwitchIcon;
-	protected readonly Mail01Icon = Mail01Icon;
-	protected readonly JavaScriptIcon = JavaScriptIcon;
-	protected readonly JavaIcon = JavaIcon;
-	protected readonly PythonIcon = PythonIcon;
-	protected readonly CrabIcon = CrabIcon;
-	protected readonly Database02Icon = Database02Icon;
-	protected readonly VisualStudioCodeIcon = VisualStudioCodeIcon;
-
-	projects: Project[] = [
-		new Project(0, "QEM Rust", "An implementation of QEM (Quadric Error Metric) in Rust.", "🚀", ["rust", "research", "3D"]),
-		new Project(1, "SunWeb", "A small web server for http and https made with Rust. Flexible for many uses.", "🚀", ["rust", "webserver", "http"]),
-		new Project(2, "DigitalClock-ESP32", " digital clock built with a ESP32", "🚀", ["c++", "esp32"]),
-		new Project(3, "LoginAPI", "A Java API for Users", "🚀", ["java", "api", "mysql"]),
-		new Project(4, "Card-Games", " Card Games written for the CLI", "🚀", ["python"])
-		]
-
-}
-
-class Project {
-	id: number;
-	name: string;
-	description: string;
-	image: string;
-	tags: string[];
-
-	constructor(id: number, name: string, description: string, image: string, tags: string[]) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.image = image;
-		this.tags = tags;
-	}
 }
